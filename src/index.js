2a7aa4875ecd1f4d9cc6044cd64060bd2f6dfc19
FrancescoLength <france.schino@live.it>
2025-12-01 16:39:58 +0000

    Update service worker cache version and enhance update handling

--- src/index.js
+++ src/index.js
@@ -21,12 +21,38 @@ reportWebVitals();
 // Register Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registered:', registration);
-      })
-      .catch(error => {
-        console.error('Service Worker registration failed:', error);
-      });
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('Service Worker registered:', registration);
+
+      registration.onupdatefound = () => {
+        const installingWorker = registration.installing;
+        if (installingWorker == null) {
+          return;
+        }
+        installingWorker.onstatechange = () => {
+          if (installingWorker.state === 'installed') {
+            if (navigator.serviceWorker.controller) {
+              // At this point, the updated precached content has been fetched,
+              // but the previous service worker will still serve the older
+              // content until all client tabs are closed.
+              console.log('New content is available and will be used when all tabs for this page are closed.');
+
+              // Execute callback
+              if (window.confirm("Nuova versione disponibile! Premi OK per aggiornare l'app.")) {
+                window.location.reload();
+              }
+            } else {
+              // At this point, everything has been precached.
+              // It's the perfect time to display a
+              // "Content is cached for offline use." message.
+              console.log('Content is cached for offline use.');
+            }
+          }
+        };
+      };
+
+    }).catch(error => {
+      console.error('Service Worker registration failed:', error);
+    });
   });
 }
