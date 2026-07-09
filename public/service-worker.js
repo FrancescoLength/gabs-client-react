66f57cad740f02c7078463a861898c768b29b336
FrancescoLength <france.schino@live.it>
2026-07-09 10:31:29 +0200

    fix: restrict service worker cache interception to GET requests only

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,6 +21,10 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
